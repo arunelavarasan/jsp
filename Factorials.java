bacef61bc47626c80413550cb9f1f65ac406498d
arunelavarasan <arunelavarasan168@gmail.com>
2022-09-15 22:10:15 +0530

    15-09-22

--- Factorials.java
+++ Factorials.java
@@ -3,7 +3,8 @@ package jsp;
 public class Factorials {
 	public static void main(String[] args)
 	{
-		for(int i=1;i<=10;i++)
+		int n = 10;
+		for(int i=1;i<=n;i++)
 		{
 			int fact=1; 
 			for(int j=1;j<=i;j++)
